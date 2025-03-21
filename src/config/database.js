7194e70ddcf2196b6a9e76658b8f8f2d6c2d9a4a
Dimas Prasetio <dimas.prasetio@neuronworks.co.id>
2025-03-22 01:07:41 +0700

    update

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'yourdatabase',
   process.env.DB_USER || 'youruser',
-  process.env.DB_PASSWORD || 'yourpassword',
+  process.env.DB_PASSWORD || '',
   {
     host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql'
